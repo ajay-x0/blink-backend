46c0951c135be4ea0e3933421d286b20705d6f1c
ajayraghuwanshi <ajayrg2401@gmail.com>
2026-08-04 18:53:17 +0530

    Initial commit

---
+++ src/main/java/com/chat_app/blink_backend/BlinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chat_app.blink_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlinkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat_app/blink_backend/BlinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat_app.blink_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlinkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
